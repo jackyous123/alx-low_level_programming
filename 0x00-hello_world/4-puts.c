72e3e507f272ee5f799c013fe24e8b2fbb0f0e13
jackyous123 <jack.yousery123@gmail.com>
2023-02-16 15:11:33 -0800

    gggg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+#include<stdio.h>
+/**
+* main - print a text string with puts
+* Return: cero
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
