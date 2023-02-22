d106300723e4df4e6177b025803fd77dce975046
jackyous123 <jack.yousery123@gmail.com>
2023-02-22 08:55:42 -0800

    fwef

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char character = 'a';
+
+	while (character <= 'z')
+	{
+		_putchar(character);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
