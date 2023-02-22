8aa026bd701c13be0368a9155f5dc972d75bac5d
jackyous123 <jack.yousery123@gmail.com>
2023-02-22 08:39:04 -0800

    dfasd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main -  prints _putchar, followed by a new line
@@ -7,8 +6,15 @@
  */
 int main(void)
 {
-	printf("\_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
-
